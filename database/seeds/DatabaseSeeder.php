ccef84d3052d3fef792d0829f7de57d427ca9692
Felipe Reksua <felipe@websix.com.br>
2019-12-05 23:33:53 -0300

    Seed email Felipe

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -39,6 +39,15 @@ class DatabaseSeeder extends Seeder
 		        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 		        'remember_token' => Str::random(10),
 	        ]);
+
 		}
+		
+        DB::table('users')->insert([
+            'name' => 'Felipe Reksua',
+	        'email' => 'reksuaf@gmail.com',
+	        'email_verified_at' => now(),
+	        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+	        'remember_token' => Str::random(10),
+        ]);
     }
 }
